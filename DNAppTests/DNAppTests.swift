52dba9cae0d1a0e74c039592a5a76533c2b2f034
robmyers82 <robert_myers_@hotmail.com>
2015-05-12 09:20:31 -0300

    Initial Commit

---
+++ DNAppTests/DNAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  DNAppTests.swift
+//  DNAppTests
+//
+//  Created by UIT Coach on 2015-05-12.
+//  Copyright (c) 2015 UIT Coach. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DNAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
